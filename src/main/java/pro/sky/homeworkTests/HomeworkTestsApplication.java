ceb5cee0a15959e287192dd78fdf945a98ce3a1a
Victor Severin <severinv@list.ru>
2023-05-12 17:05:19 +0300

    initial commit

---
+++ src/main/java/pro/sky/homeworkTests/HomeworkTestsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworkTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkTestsApplication.class, args);
+	}
+
+}
